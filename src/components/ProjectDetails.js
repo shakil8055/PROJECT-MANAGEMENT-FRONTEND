dfb28e46b6a81cde0fbb08f206ed926e7e5232f6
shakil8055 <mohamedshakil883@gmail.com>
2024-09-11 11:48:41 +0530

    commit

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -39,102 +39,105 @@ const ProjectDetails = ({ project }) => {
     setIsOverlayOpen(true);
   };
 
-  const handleOverlay = () => {
+  const closeModal = () => {
     setIsModalOpen(false);
     setIsOverlayOpen(false);
   };
 
   return (
-    
-    <div className="project bg-slate-800 item-center items-stretch p-5 rounded-xl border border-slate-700 flex flex-col mx-5 w-[28rem]">
-      <div className="project-top">
-        <span className="text-sky-400">{project._id}</span>
-        <h3 className="text-3xl font-medium truncate">{project.title}</h3>
-        <span className="uppercase text-xs tracking-widest text-slate-500 font-medium">
-          {project.tech}
-        </span>
-        <div className="text-slate-300 mb-3"><span>{project.description}</span></div>
-        <hr></hr>
-      </div>
-
-      <div className="project-mid mt-2 flex text-sm text-slate-300 gap-5">
-        <div className="left flex flex-col">
-          <span>Budget : {currencyFormatter(project.budget)}</span>
-         
-          <span>
-            Added : {moment(project.createdAt).format("MMM DD, hh:mm A")}
+    <div className="relative">
+      <div className="project bg-slate-800 item-center items-stretch p-5 rounded-xl border border-slate-700 flex flex-col mx-5 w-[28rem]">
+        <div className="project-top">
+          <span className="text-sky-400">{project._id}</span>
+          <h3 className="text-3xl font-medium truncate">{project.title}</h3>
+          <span className="uppercase text-xs tracking-widest text-slate-500 font-medium">
+            {project.tech}
           </span>
-          <span>
-            Updated : {moment(project.updatedAt).format("MMM DD, hh:mm A")}
-          </span>
+          <div className="text-slate-300 mb-3">
+            <span>{project.description}</span>
+          </div>
+          <hr></hr>
         </div>
-        <div className="right flex flex-col">
-        <span>Team Leader : {project.manager}</span>
-          <span>
-            Duration :{" "}
-            {`${project.duration} week${project.duration === 1 ? "" : "s"}`}
-          </span>
-        </div>
-      </div>
 
-      <div className="project-bottom justify-end flex mt-2 gap-5">
-        <button
-          onClick={handleUpdate}
-          className="text-sky-500  hover:underline"
-        >
-          Update
-        </button>
-        <button
-          onClick={handleDelete}
-          className="text-rose-500 hover:underline"
-        >
-          Delete
-        </button>
+        <div className="project-mid mt-2 flex text-sm text-slate-300 gap-5">
+          <div className="left flex flex-col">
+            <span>Budget : {currencyFormatter(project.budget)}</span>
+            <span>
+              Added : {moment(project.createdAt).format("MMM DD, hh:mm A")}
+            </span>
+            <span>
+              Updated : {moment(project.updatedAt).format("MMM DD, hh:mm A")}
+            </span>
+          </div>
+          <div className="right flex flex-col">
+            <span>Team Leader : {project.manager}</span>
+            <span>
+              Duration :{" "}
+              {`${project.duration} week${project.duration === 1 ? "" : "s"}`}
+            </span>
+          </div>
+        </div>
+
+        <div className="project-bottom justify-end flex mt-2 gap-5">
+          <button
+            onClick={handleUpdate}
+            className="text-sky-500 hover:underline"
+          >
+            Update
+          </button>
+          <button
+            onClick={handleDelete}
+            className="text-rose-500 hover:underline"
+          >
+            Delete
+          </button>
+        </div>
       </div>
 
       {/* OVERLAY */}
-      <div
-        onClick={handleOverlay}
-        className={`overlay z-[1] h-screen w-screen bg-slate-900/50 backdrop-blur-sm top-0 left-0 right-0 bottom-0 ${
-          isOverlayOpen ? "" : "hidden"
-        }`}
-      ></div>
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-slate-800 bg-opacity-75 flex items-center justify-center z-50">
+          {/* MODAL */}
+          <div
+            className={`update-modal h-[40rem] w-[35rem] bg-slate-800 p-10 rounded-xl shadow-xl border border-slate-700 z-50 flex flex-col relative`}
+          >
+            <h2 className="text-3xl font-bold mb-3 text-sky-400">
+              Update Project
+            </h2>
 
-      {/* MODAL */}
-      <div
-        className={`update-modal h-[40rem] w-[35rem] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-800 p-10 rounded-xl shadow-xl border border-slate-700 z-[2] flex flex-col  ${
-          isModalOpen ? "" : "hidden"
-        }`}
-      > 
-      
-        <h2 className="text-3xl font-bold mb-3 text-sky-400">
-            Update Project
-        </h2>
-        <div
-    style={{
-      flex: 1,
-      overflow: 'auto',
-      scrollbarWidth: 'none', 
-      msOverflowStyle: 'none' 
-    }}
-  >
-    <div
-      style={{
-        overflow: 'auto',
-        WebkitOverflowScrolling: 'touch' 
-      }}
-    >
-      <ProjectForm
-          project={project}
-          setIsModalOpen={setIsModalOpen}
-          setIsOverlayOpen={setIsOverlayOpen}
-        />
+            {/* Close Button */}
+            <button
+              onClick={closeModal}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-300 text-3xl p-2"
+            >
+              &times;
+            </button>
+
+            <div
+              style={{
+                flex: 1,
+                overflow: "auto",
+                scrollbarWidth: "none",
+                msOverflowStyle: "none",
+              }}
+            >
+              <div
+                style={{
+                  overflow: "auto",
+                  WebkitOverflowScrolling: "touch",
+                }}
+              >
+                <ProjectForm
+                  project={project}
+                  setIsModalOpen={setIsModalOpen}
+                  setIsOverlayOpen={setIsOverlayOpen}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-        
-      </div>
-    </div>
-    
   );
 };
 
